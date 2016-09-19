1575d9dcbf087775e2dea9f4a1dd4ebcf404a630
kiricon <kiricon@live.com>
2016-09-18 21:44:53 -0600

    Built out a system for parsing out the input file in to a list of engines and queries.

--- 2008/A:Saving the Universe/main.dart
+++ 2008/A:Saving the Universe/main.dart
@@ -2,11 +2,11 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 
-void main() async{
+main() async{
   String content = await readFile("input.txt");
   List<String> lineList = parseByLine(content);
-  print(lineList);
-  writeFile("test.txt", "hello \nworld");
+  parse(lineList);
+  //print(lineList);
 }
 
 
@@ -15,7 +15,7 @@ Future readFile(String fileName) async{
   return await(file.readAsString(encoding: ASCII));
 }
 
-List<string> parseByLine(String line){
+List<String> parseByLine(String line){
   return line.trim().split('\n');
 }
 
@@ -23,4 +23,41 @@ void writeFile(String name, String content){
   var writeFile = new File(name).openWrite();
   writeFile.write(content);
   writeFile.close();
+}
+
+void parse(List<String> file){
+
+  int cases = int.parse(file[0]);
+  print(file[1]);
+  int index = 1;
+  List<List<String>> allEngines = new List<List<String>>();
+  List<List<String>> allQueries = new List<List<String>>();
+
+  for(int x = 1; x <= cases; x++){
+
+    List<String> engines = new List<String>();
+    List<String> queries = new List<String>();
+    int curr;
+
+    curr = index;
+    for(int i = curr+1; i <= curr+int.parse(file[curr]); i++){
+      //print(i);
+      engines.add(file[i]);
+      index = i;
+     // print(index);
+    }
+    index += 1;
+    curr = index;
+    for(int i = curr+1; i <= curr+int.parse(file[curr]); i++){
+      queries.add(file[i]);
+      index = i;
+    } 
+    index+=1;
+
+    allEngines.add(engines);
+    allQueries.add(queries);
+
+  }
+
+
 }

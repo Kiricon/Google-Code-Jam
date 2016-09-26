5f34544a9f54f91d3e789a9343575eeaf89d78dd
kiricon <kiricon@live.com>
2016-09-25 19:14:37 -0600

    Made changes so that it removes the matches as it goes. Thus making better choices.

--- 2008/A:Save The Universe/A:Save The Universe/main.cpp
+++ 2008/A:Save The Universe/A:Save The Universe/main.cpp
@@ -11,29 +11,33 @@ using namespace std;
 
 vector<string> split(string str, string sep);
 string readFile(string fileName);
+void writeFile(string);
 
 class Case {
 public:
   vector<string> engines, queries;
 };
 
+class Match {
+public:
+  int index;
+  int matches;
+};
+
 vector<vector<int> > findMatches(vector<Case>);
 vector<Case> parse(vector<string> arr);
 string solve(vector<vector<int> > totalMatches, vector<Case> cases);
-
+int pickEngine(vector<int>, int, vector<string>, string);
 
 
 int main() {
   //cout << readFile("Input.txt").split("\n"));
-  vector<string> arr = split(readFile("input.txt"), "\n");
+  vector<string> arr = split(readFile("A-small-practice.in"), "\n");
   vector<Case> runs = parse(arr);
   //string output = solve(runs);
   vector<vector <int> > totalMatches = findMatches(runs);
-  solve(totalMatches, runs);
-  /*
-  for(int i = 0; i < sizeof(arr); i++){
-    cout << "Line: "<< arr[i] << "\n";
-  } */
+  writeFile(solve(totalMatches, runs));
+
   return 0;
 }
 
@@ -41,12 +45,65 @@ int main() {
 string solve(vector<vector<int> > totalMatches, vector<Case> cases){
 
   string answer = "";
-
+  for(int i = 0; i < cases.size(); i++){
+    
+    int switches = 0;
+    if(cases[i].queries.size() > 0){
+    int engineIndex = pickEngine(totalMatches[i], int(totalMatches[i].size()), cases[i].engines, cases[i].queries[0]);
+    
+    string output = "";
+    
+    for(int q = 0; q < cases[i].queries.size(); q++){
+      if(cases[i].queries[q] == cases[i].engines[engineIndex]){
+        totalMatches[i][engineIndex]--;
+        switches++;
+        engineIndex = pickEngine(totalMatches[i], engineIndex, cases[i].engines, cases[i].queries[q]);
+        
+      }else{
+        for( int z = 0; z < cases[i].engines.size(); z++){
+          if(cases[i].engines[z] == cases[i].queries[q]){
+            totalMatches[i][z]--;
+          }
+        }
+      }
+    }
+    }
+    
+    
+    
+    answer += "Case #"+to_string(i+1)+": "+to_string(switches)+"\n";
+  }
 
 
   return answer;
 }
 
+void writeFile(string output){
+  
+  ofstream myfile;
+  myfile.open("ouput.txt");
+  myfile << output;
+  myfile.close();
+  
+}
+int pickEngine(vector<int> matches, int last, vector<string> engines, string query){
+  
+  int index = 0;
+  int bestPick = 30000;
+  
+  for(int i = 0; i < matches.size(); i++){
+    if(matches[i] < bestPick && i != last  && engines[i] != query){
+      index = i;
+      bestPick = matches[i];
+    }
+  }
+  
+  return index;
+  
+}
+
+
+//Find the number of matches per engine.
 vector<vector<int> > findMatches(vector<Case> Cases){
 
   vector<vector<int> > allMatches;
@@ -89,33 +146,35 @@ vector<vector<int> > findMatches(vector<Case> Cases){
 
 //Parse out the array of string delemited by line in to cases, queries and engines
 vector<Case> parse ( vector<string> arr){
-
+  
   //int cases = atoi(arr[0].c_str());
   int cases = atoi(arr[0].c_str());
   int index = 0;
   int curr, no;
-  vector<Case> run(2);
-
+  vector<Case> run(cases);
+  
   for(int c = 0; c < cases; c++){
-
+    
     //Find Search engines for specific case
     curr = index+1;
     no = atoi(arr[curr].c_str()) + curr;
-
+    
     for(int i = curr+1; i <= no; i++ ){
       run[c].engines.push_back(arr[i]);
       index = i;
     }
-
+    
     //Find Search queries for specific case
     curr = index+1;
     no = atoi(arr[curr].c_str()) + curr;
-
+    
     for(int i = curr; i <= no; i++ ){
+      if(i != curr){
       run[c].queries.push_back(arr[i]);
+      }
       index = i;
     }
-
+    
   }
   return run;
 }

831578a99b96344a6d1709c9bbd43832b8fb14b9
kiricon <kiricon@live.com>
2016-09-25 21:12:18 -0600

    Built out basic case parsing system.

--- 2008/Q:B. Train Timetable/Q:B. Train Timetable/main.cpp
+++ 2008/Q:B. Train Timetable/Q:B. Train Timetable/main.cpp
@@ -7,10 +7,100 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
 using namespace std;
 
+class Case {
+public:
+    int turnaround, starta, startb;
+    vector<string> na, nb;
+};
+
+vector<string> readFileByLine(string);
+vector<Case> parse(vector<string>);
+vector<string> split(string, string);
+
+// ###### MAIN  ##########
 int main(int argc, const char * argv[]) {
     
-    cout << "Hello, World!\n";
+    parse(readFileByLine("input.txt"));
     return 0;
+}
+
+
+//####### Parse the lines in to cases ######
+vector<Case> parse(vector<string> lines){
+    
+    int noCases = atoi(lines[0].c_str());
+    vector<Case> cases(noCases);
+    int index = 1;
+    int curr;
+    
+    for(int c = 0; c < cases.size(); c++){
+        
+        cases[c].turnaround = atoi(lines[index].c_str());
+        index++;
+        
+        vector<string> timeSlots = split(lines[index], " ");
+        int naNum = atoi(timeSlots[0].c_str());
+        int nbNum;
+        if(timeSlots.size() == 1){
+            nbNum = 0;
+        }else{
+            nbNum = atoi(timeSlots[1].c_str());
+        }
+        int total =  naNum + nbNum;
+        index++;
+        
+        curr = index;
+        for(int i = curr; i < curr+total; i++){
+            
+            if(i < curr+naNum){
+                cases[c].na.push_back(lines[i]);
+            }else{
+                cases[c].nb.push_back(lines[i]);
+            }
+
+            index = i;
+        }
+        index++;
+        
+        
+    }
+    
+    
+    
+    return cases;
+}
+
+
+
+// ########## Read the Input file and return vector of lines ####
+vector<string> readFileByLine(string name){
+    vector<string> lines;
+    ifstream fs(name);
+    string line;
+    while (getline(fs, line)) {
+        lines.push_back(line);
+    }
+    return lines;
+}
+
+// Splits a string by a delimeter
+vector<string> split(string str, string sep){
+    char* cstr=const_cast<char*>(str.c_str());
+    char* current;
+    vector<string> arr;
+    current=strtok(cstr,sep.c_str());
+    while(current != NULL){
+        arr.push_back(current);
+        current=strtok(NULL, sep.c_str());
+    }
+    return arr;
 }

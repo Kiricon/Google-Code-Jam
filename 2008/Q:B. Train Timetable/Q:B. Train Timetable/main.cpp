b6a5eb3d55b5a48f9a464b4183f9f4d332beb46b
kiricon <kiricon@live.com>
2016-09-25 20:14:31 -0600

    Made train timetable project

---
+++ 2008/Q:B. Train Timetable/Q:B. Train Timetable/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Q:B. Train Timetable
+//
+//  Created by Dominic Valenciana on 9/25/16.
+//  Copyright © 2016 Dominic Valenciana. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "Hello, World!\n";
+    return 0;
+}
